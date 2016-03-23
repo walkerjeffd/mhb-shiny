34d8d9c5d53bf446f07727a898236c7d9c38270c
Jeff Walker <walker.jeff.d@gmail.com>
2016-03-23 08:35:38 -0400

    add flag-resamples and analyze-resamples scripts

---
+++ analyze-resamples.R
@@ -0,0 +1,143 @@
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+theme_set(theme_bw())
+
+# analyze the resamples using the data frame created from flag-resamples
+
+calweek <- function (x) {
+  y <- week(x + days(wday(as.Date(paste(year(x), "01", "01", sep="-")))-1))
+  y <- y + ifelse(y == 1 & month(x)==12, 52, 0)
+  y
+}
+
+wq <- readRDS('data-wq-resamples.Rdata') %>%
+  arrange(SAMPLE_POINT_NAME, SAMPLE_DATE) %>%
+  mutate(GT_104 = ENTEROCOCCI > 104,
+         GT_70 = ENTEROCOCCI > 70,
+         BW_70_104 = GT_70 * !(GT_104))
+
+# check that all resamples follow an exceedence
+wq %>%
+  group_by(SAMPLE_POINT_NAME) %>%
+  mutate(PREV_GT_104=lag(GT_104)) %>%
+  filter(RESAMPLE, !PREV_GT_104) %>%
+  select(SAMPLE_POINT_NAME, id, SAMPLE_DATE, WEEK) %>%
+  nrow # should be 0
+
+# compute fraction of routine samples between 70-104 by site and year
+bav_site_year <- filter(wq, !RESAMPLE, !OTHER) %>%
+  group_by(SAMPLE_POINT_NAME, YEAR) %>%
+  summarise(N = n(),
+            N_GT_104 = sum(GT_104),
+            N_GT_70 = sum(GT_70),
+            N_BW_70_104 = sum(BW_70_104),
+            FRAC_BW_70_104 = N_BW_70_104/N) %>%
+  ungroup
+arrange(bav_site_year, desc(FRAC_BW_70_104)) %>% View
+
+# compute fraction of samples between 70-104 by site and year
+bav_site <- bav_site_year %>%
+  group_by(SAMPLE_POINT_NAME) %>%
+  summarise(MIN_FRAC_BW_70_104=min(FRAC_BW_70_104),
+            MAX_FRAC_BW_70_104=max(FRAC_BW_70_104),
+            MEAN_FRAC_BW_70_104=mean(FRAC_BW_70_104),
+            MEDIAN_FRAC_BW_70_104=median(FRAC_BW_70_104),
+            N_YEAR=n(),
+            N_SAMPLE=sum(N),
+            MEAN_N_SAMPLE_PER_YEAR=mean(N),
+            MEDIAN_N_SAMPLE_PER_YEAR=median(N),
+            # mean/median/max number of additional resamples per year under BAV
+            MEAN_N_RESAMPLE_PER_YEAR=MEAN_FRAC_BW_70_104*MEAN_N_SAMPLE_PER_YEAR,
+            MEDIAN_N_RESAMPLE_PER_YEAR=MEDIAN_FRAC_BW_70_104*MEDIAN_N_SAMPLE_PER_YEAR,
+            MAX_N_RESAMPLE_PRE_YEAR=MAX_FRAC_BW_70_104*MEAN_N_SAMPLE_PER_YEAR)
+
+
+arrange(bav_site, desc(MEDIAN_N_RESAMPLE_PER_YEAR)) %>% View
+
+# highest median number of additional resamples is almost 3 (WIL-02)
+stn <- "WIL-02"
+filter(wq, SAMPLE_POINT_NAME==stn, !RESAMPLE, !OTHER) %>%
+  mutate(COLOR = ifelse(ENTEROCOCCI > 104, ">104",
+                        ifelse(ENTEROCOCCI > 70, "70-104", "<70")),
+         COLOR = ordered(COLOR, levels=c("<70", "70-104", ">104"))) %>%
+  ggplot(aes(WEEK, WEEKDAY, fill=COLOR)) +
+  geom_tile() +
+  scale_fill_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="red")) +
+  scale_x_discrete(drop = FALSE) +
+  scale_y_discrete(breaks=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"), drop = FALSE) +
+  facet_wrap(~YEAR, nrow=5) +
+  labs(x="Week of Year", y="Day of Week",
+       title=paste0("SAMPLE_POINT_NAME=", stn)) +
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
+
+
+# median total number of samples per year (across all sites)
+# 1151
+sum(bav_site$MEDIAN_N_SAMPLE_PER_YEAR)
+
+# mean total number of samples per year (across all sites)
+# 1140
+sum(bav_site$MEAN_N_SAMPLE_PER_YEAR)
+
+# median total number of samples per year (across all sites)
+# 24 (2% increase in number of samples)
+sum(bav_site$MEDIAN_N_RESAMPLE_PER_YEAR)
+
+# mean number of additional resamples per year under BAV (across all sites)
+# 42 (3.7% increase in number of samples)
+sum(bav_site$MEAN_N_RESAMPLE_PER_YEAR)
+
+
+
+#
+# filter(wq, SAMPLE_POINT_NAME=="EEB-01", !RESAMPLE, !OTHER) %>%
+#   ggbrush()
+
+
+# compute fraction of historical resamples
+# that were exceedences
+x <- arrange(wq, SAMPLE_POINT_NAME, SAMPLE_DATE) %>%
+  mutate(DATE = as.Date(SAMPLE_DATE)) %>%
+  group_by(SAMPLE_POINT_NAME, YEAR) %>%
+  mutate(PREV_GT_104=lag(GT_104),
+         PREV_ENT=lag(ENTEROCOCCI),
+         DIFFTIME = as.numeric(difftime(DATE, lag(DATE), units = "days"))) %>%
+  ungroup %>%
+  filter(RESAMPLE)
+
+# plot of resample concentration versus previous concentration (all of which were exceedences)
+x %>%
+  ggplot(aes(PREV_ENT, ENTEROCOCCI, color=GT_104)) +
+  geom_point() +
+  facet_wrap(~DIFFTIME) +
+  scale_x_log10() +
+  scale_y_log10()
+
+# fraction of resamples that were exceedences by site
+# where there were at least 10 resamples overall
+# to exclude sites with very few resamples (generally clean)
+filter(x, DIFFTIME %in% c(1, 2)) %>%
+  group_by(SAMPLE_POINT_NAME) %>%
+  summarise(N=n(),
+            N_GT_104 = sum(GT_104),
+            FRAC=N_GT_104/N) %>%
+  filter(N>10) %>%
+  arrange(desc(FRAC))
+
+# highest fraction is LIN-3
+# plot shows a lot of consecutive exceedences
+stn <- "LIN-3"
+filter(wq, SAMPLE_POINT_NAME==stn) %>%
+  mutate(COLOR = ifelse(ENTEROCOCCI > 104, ">104",
+                        ifelse(ENTEROCOCCI > 70, "70-104", "<70")),
+         COLOR = ordered(COLOR, levels=c("<70", "70-104", ">104"))) %>%
+  ggplot(aes(WEEK, WEEKDAY, fill=COLOR)) +
+  geom_tile() +
+  scale_fill_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="red")) +
+  scale_x_discrete(drop = FALSE) +
+  scale_y_discrete(breaks=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"), drop = FALSE) +
+  facet_wrap(~YEAR, nrow=5) +
+  labs(x="Week of Year", y="Day of Week",
+       title=paste0("SAMPLE_POINT_NAME=", stn)) +
+  theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
---
+++ flag-resamples.R
@@ -0,0 +1,150 @@
+library(shiny)
+library(miniUI)
+library(ggplot2)
+library(dplyr)
+library(lubridate)
+
+# This script was created to manually flag any samples considered a "resample" or "other"
+# Resamples are defined as a sample collected within a couple days of a previous exceedence
+#   note that sometimes resamples are collected in the following week (e.g. if the sample
+#   on friday was an exceedence, they might not resample until monday)
+# "Other" samples are those that are not resamples and do not appear to be "routine"
+#   there are some years and sites where samples were collected irregularly
+#   other samples were flagged to leave only the regular samples for analysis
+
+calweek <- function (x) {
+  y <- week(x + days(wday(as.Date(paste(year(x), "01", "01", sep="-")))-1))
+  y <- y + ifelse(y == 1 & month(x)==12, 52, 0)
+  y
+}
+
+wq <- readRDS('data-wq.Rdata') %>%
+  mutate(id = row_number()) %>%
+  mutate(YEAR=year(SAMPLE_DATE),
+         WEEK=as.character(calweek(SAMPLE_DATE)),
+         WEEKDAY=as.character(wday(SAMPLE_DATE))) %>%
+  mutate(WEEK=ordered(WEEK, levels=as.character(seq(20, 46))),
+         WEEKDAY=plyr::revalue(WEEKDAY, replace = c("1"="Sun", "2"="Mon", "3"="Tue", "4"="Wed", "5"="Thu", "6"="Fri", "7"="Sat"), warn_missing = FALSE),
+         WEEKDAY=factor(WEEKDAY, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))) %>%
+  filter(!is.na(ENTEROCOCCI))
+
+stations <- sort(unique(wq$SAMPLE_POINT_NAME))
+
+
+# Shiny Gadget
+#   data: data frame containing all samples for a single station (stn)
+#   click: when a sample is clicked, the id is printed to the console and the
+#          id and stn are appended to the `results` data frame (which is a global)
+ggbrush <- function(data, stn) {
+  ui <- miniPage(
+    gadgetTitleBar("Click to select a sample point"),
+    miniContentPanel(
+      plotOutput("plot", height = "100%", click = "plot_click")
+    )
+  )
+
+  server <- function(input, output, session) {
+    output$plot <- renderPlot({
+      data %>%
+      ggplot(aes(WEEK, WEEKDAY, color=(ENTEROCOCCI > 104))) +
+        geom_point(size=4) +
+        geom_point(fill=NA, color="cyan", shape=21, size=4, data=filter(data, id %in% resamples$id)) +
+        geom_point(fill=NA, color="orange", shape=21, size=4, data=filter(data, id %in% others$id)) +
+        scale_color_manual("Exceeds 104", values=c("FALSE"="grey50", "TRUE"="red")) +
+        scale_x_discrete(drop = FALSE) +
+        scale_y_discrete(breaks=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"), drop = FALSE) +
+        facet_wrap(~YEAR, nrow=5) +
+        labs(x="Week of Year", y="Day of Week",
+             title=paste0("SAMPLE_POINT_NAME=", stn)) +
+        theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5)) +
+        theme_bw()
+    })
+
+    observe({
+      x <- nearPoints(data, input$plot_click)
+      if (nrow(x) == 1) {
+        # only record the click if there is one sample returned
+        id <- x[["id"]][1]
+        stn <- x[["SAMPLE_POINT_NAME"]][1]
+        results <<- rbind(results, data.frame(id=id, stn=stn))
+        cat("Added id = ", id, "\n")
+      } else if (nrow(x) > 1) {
+        cat("TOO MANY POINTS", "\n")
+      } else {
+        cat("NOTHING CLICKED", "\n")
+      }
+    })
+
+    observeEvent(input$done, {
+      # return the results data frame on "Done"
+      stopApp(results)
+    })
+
+    observeEvent(input$cancel, {
+      # return nothing on "Cancel"
+      stopApp(NULL)
+    })
+  }
+
+  runGadget(ui, server)
+}
+
+# initialize results data frame
+results <- data.frame()
+
+# loop through each station and open the widget
+# click Done to go to the next station
+# click Cancel to stop the loop
+for (stn in stations) {
+  cat("Station:", stn, "\n")
+  x <- filter(wq, SAMPLE_POINT_NAME==stn) %>%
+    ggbrush()
+  if (is.null(x)) {
+    break
+  }
+}
+
+# the following variables are iterative changes to the resamples array
+# note that these are NOT reproducible and only worked when I first did
+# this because the results data frame will be different
+resamples_1 <- results
+resamples_2 <- rbind(resamples_1, filter(results, id %in% c(11191, 6254, 6260)))
+resamples_3 <- filter(resamples_2, !(id %in% c(7281, 526, 9719)))
+resamples_4 <- filter(resamples_3, !(id %in% others_3$id))
+resamples_5 <- filter(resamples_4, !(id %in% c(4880, 6206, 3846, 7337, 8093)))
+resamples_6 <- rbind(resamples_5, filter(results, id %in% c(7280, 3849, 7338, 2993, 3518, 2780, 8094)))
+resamples_7 <- filter(resamples_6, !(id %in% c(6377, 2563)))
+resamples_8 <- filter(resamples_7, !(id %in% c(7619, 7611, 7637)))
+resamples_9 <- filter(resamples_8, !(id %in% c(533, 7452, 7479, 7554, 7540, 7574, 4722, 5284, 8411, 3799, 9634, 11815)))
+resamples_10 <- rbind(resamples_9, filter(results, id == 3801))
+resamples_11 <- filter(resamples_10, id != 4771)
+resamples_12 <- filter(resamples_11, id != 10323)
+
+# the following variables are iterative changes to the other array
+# same caveat applies
+others_1 <- results
+others_2 <- rbind(others_1, filter(results, id %in% c(7667, 13165, 11145)))
+others_3 <- filter(others_2, !(id %in% c(7281, 526, 9719)))
+others_4 <- filter(others_3, !(id == 8851))
+others_5 <- rbind(others_4, filter(results, id %in% c(2563, 6377)))
+others_6 <- rbind(others_5, filter(results, id %in% c(7619, 7611, 7637)))
+others_7 <- rbind(others_6, filter(results, id == 10323))
+others_8 <- rbind(others_7, filter(results, id == 4771))
+
+# copy to primary data frames
+others <- others_8
+resamples <- resamples_12
+
+# save flagged ids
+write.csv(resamples, file="resamples.csv", row.names=FALSE)
+write.csv(others, file="others.csv", row.names=FALSE)
+
+# apply resample and others to wq data frame
+wq <- mutate(wq,
+             RESAMPLE = id %in% resamples$id,
+             OTHER = id %in% others$id)
+
+# save updated wq data frame
+write.csv(wq, file="wq-with-resamples.csv", row.names=FALSE)
+saveRDS(wq, file="data-wq-resamples.Rdata")
+
