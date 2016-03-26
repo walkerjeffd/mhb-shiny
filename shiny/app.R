d60b9367a1f254e543c6c1cc5424a6f8b5e7f60e
Jeff Walker <walker.jeff.d@gmail.com>
2016-03-26 18:11:03 -0400

    update shiny app with overview plot, checkboxes for type

--- shiny/app.R
+++ shiny/app.R
@@ -3,9 +3,11 @@ library(shinydashboard)
 library(DT)
 library(leaflet)
 library(dplyr)
+library(tidyr)
 library(lubridate)
 library(ggplot2)
-theme_set(theme_bw())
+theme_set(theme_bw(base_size=16))
+
 
 calweek <- function (x) {
   y <- week(x + days(wday(as.Date(paste(year(x), "01", "01", sep="-")))-1))
@@ -30,11 +32,14 @@ log_labels <- function(x, y) {
 # -------------------------------------
 # SET UP DATASETS
 # -------------------------------------
-wq <- readRDS('data-wq.Rdata') %>%
-  mutate(YEAR=year(SAMPLE_DATE),
-         WEEK=calweek(SAMPLE_DATE),
-         WEEKDAY=wday(SAMPLE_DATE),
-         EXCEED_GRP=cut(ENTEROCOCCI, breaks=c(0, 70, 104, Inf)),
+stn <- readRDS('data-stn.Rdata')
+wq <- readRDS('data-wq-resamples.Rdata')
+
+wq <- wq %>%
+  mutate(SAMPLE_TYPE = ifelse(RESAMPLE, "Resample",
+                              ifelse(OTHER, "Irregular", "Routine")),
+         SAMPLE_TYPE = ordered(SAMPLE_TYPE, levels=c("Routine", "Resample", "Irregular"))) %>%
+  mutate(EXCEED_GRP=cut(ENTEROCOCCI, breaks=c(0, 70, 104, Inf)),
          EXCEED_GRP=plyr::revalue(as.character(EXCEED_GRP),
                                   c("(0,70]"="<70",
                                     "(70,104]"="70-104",
@@ -48,7 +53,109 @@ wq <- readRDS('data-wq.Rdata') %>%
          PAST_WEATHER = ordered(PAST_WEATHER, levels=c("NO RAIN", "LIGHT RAIN", "MEDIUM RAIN", "HIGH RAIN")),
          CURRENT = ordered(CURRENT, levels=c("SLOW CURRENT", "MEDIUM CURRENT", "RAPID CURRENT")))
 
-stn <- readRDS('data-stn.Rdata')
+
+wq2 <- wq %>%
+  arrange(SAMPLE_POINT_NAME, SAMPLE_DATE) %>%
+  mutate(GT_104 = ENTEROCOCCI > 104,
+         GT_70 = ENTEROCOCCI > 70,
+         BW_70_104 = GT_70 * !(GT_104))
+
+
+wq2 <- left_join(wq2, select(stn, SAMPLE_POINT_NAME, TOWN, SITE_NAME) %>% unique, by="SAMPLE_POINT_NAME")
+wq.yr <- filter(wq2, !RESAMPLE, !OTHER) %>%
+  group_by(SITE_NAME, SAMPLE_POINT_NAME, YEAR) %>%
+  summarise(N=n(),
+            N_104=sum(GT_104),
+            N_70=sum(GT_70)-N_104,
+            N_CLEAN=N-N_104-N_70) %>%
+  ungroup %>%
+  mutate(F_104 = N_104/N,
+         F_70 = N_70/N,
+         F_CLEAN = N_CLEAN/N)
+
+wq.yr.site <- select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+  group_by(SAMPLE_POINT_NAME) %>%
+  summarise(MEAN_N_104=mean(N_104),
+            MEAN_N_70=mean(N_70),
+            TOTAL=MEAN_N_104+MEAN_N_70) %>%
+  ungroup %>%
+  arrange(desc(TOTAL)) %>%
+  mutate(SAMPLE_POINT_NAME = ordered(SAMPLE_POINT_NAME, levels=unique(SAMPLE_POINT_NAME))) %>%
+  gather(VARIABLE, VALUE, MEAN_N_104, MEAN_N_70)
+#
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, F_CLEAN, F_70, F_104) %>%
+#   gather(VAR, VALUE, F_CLEAN:F_104) %>%
+#   ggplot(aes(SAMPLE_POINT_NAME, VALUE, fill=VAR)) +
+#   geom_boxplot(position='dodge')
+#
+# theme_set(theme_bw(base_size=16))
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+#   group_by(SAMPLE_POINT_NAME) %>%
+#   summarise(MEAN_N_70=mean(N_70)) %>%
+#   ungroup %>%
+#   arrange(MEAN_N_70) %>%
+#   mutate(SAMPLE_POINT_NAME = ordered(SAMPLE_POINT_NAME, levels=unique(SAMPLE_POINT_NAME))) %>%
+#   ggplot(aes(SAMPLE_POINT_NAME, MEAN_N_70)) +
+#   geom_bar(fill="deepskyblue", stat="identity") +
+#   scale_y_continuous(breaks=seq(0, 2, 0.5)) +
+#   coord_flip() +
+#   labs(x="Sampling Site", y="Mean # Additional Resamples per Year") +
+#   theme(axis.text.y=element_text(size=6))
+#
+# # number samples by year
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+#   group_by(SAMPLE_POINT_NAME) %>%
+#   summarise(MEAN_N_104=mean(N_104),
+#             MEAN_N_70=mean(N_70),
+#             TOTAL=MEAN_N_104+MEAN_N_70) %>%
+#   ungroup %>%
+#   arrange(TOTAL) %>%
+#   mutate(SAMPLE_POINT_NAME = ordered(SAMPLE_POINT_NAME, levels=unique(SAMPLE_POINT_NAME))) %>%
+#   gather(VARIABLE, VALUE, MEAN_N_104, MEAN_N_70) %>%
+#   ggplot(aes(SAMPLE_POINT_NAME, VALUE, fill=VARIABLE)) +
+#   geom_bar(stat="identity") +
+#   scale_fill_manual("",
+#                     values=c("MEAN_N_104"="orangered", "MEAN_N_70"="deepskyblue"),
+#                     labels=c("MEAN_N_104"="> 104 (SSM)", "MEAN_N_70"="> 70 (BAV)")) +
+#   scale_y_continuous(breaks=seq(0, 6, 1)) +
+#   coord_flip() +
+#   labs(x="Sampling Site", y="Mean # Resamples per Year") +
+#   theme(axis.text.y=element_text(size=6))
+#
+# # number resamples by date
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+#   group_by(YEAR) %>%
+#   summarise(SUM_N_70=sum(N_70)) %>%
+#   ungroup %>%
+#   mutate(YEAR = ordered(YEAR, levels=2015:2006)) %>%
+#   ggplot(aes(factor(YEAR), SUM_N_70)) +
+#   geom_bar(fill="deepskyblue", stat="identity") +
+#   geom_text(aes(label=SUM_N_70), hjust=1, nudge_y=-2, size=8, color="white") +
+#   geom_hline(yintercept=40.9, linetype="dashed") +
+#   scale_y_continuous(breaks=seq(0, 70, 5)) +
+#   coord_flip() +
+#   labs(x="Year", y="# Additional Resamples")
+#
+# # average # additional samples per year
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+#   group_by(SAMPLE_POINT_NAME) %>%
+#   summarise(MEAN_N_70=mean(N_70)) %>%
+#   summarise(SUM=sum(MEAN_N_70))
+
+# select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+#   group_by(SAMPLE_POINT_NAME) %>%
+#   summarise(MEAN_N_70=mean(N_70)) %>%
+#   summarise(SUM=sum(MEAN_N_70))
+#
+
+
+# idx <- which(wq$SAMPLE_POINT_NAME=="CAM-02" & wq$YEAR==2011 & wq$WEEKDAY=="Mon")
+# wq[idx, "RESAMPLE"] <- TRUE
+#
+# idx <- which(wq$SAMPLE_POINT_NAME=="CAM-02" & wq$YEAR==2007 & wq$WEEKDAY=="Thu")
+# wq[idx, "RESAMPLE"] <- TRUE
+
+
 stat_by_site <- filter(wq, !is.na(ENTEROCOCCI)) %>%
   group_by(SAMPLE_POINT_NAME) %>%
   summarise(COUNT       = n(),
@@ -88,10 +195,8 @@ ui <- dashboardPage(
   dashboardHeader(title = "ME Healthy Beaches"),
   dashboardSidebar(
     sidebarMenu(
-      sliderInput(inputId = 'ssm', min = 50, max = 200, value = 104, label = "Current SSM (MPN/100 mL):"),
-      sliderInput(inputId = 'bav', min = 50, max = 200, value = 70, label = "Proposed BAV (MPN/100 mL):"),
       menuItem("Station Explorer", tabName = "station", icon = icon("map-marker")),
-      menuItem("Station Comparisons", tabName = "overview", icon = icon("globe")),
+      menuItem("Overview Comparisons", tabName = "overview", icon = icon("globe")),
       menuItem("About", tabName = "about", icon = icon("info-circle"))
     ),
     tags$ul(class="sidebar-menu",
@@ -138,22 +243,36 @@ ui <- dashboardPage(
               tableOutput("stnInfo")
             ),
             tabPanel("Timeseries",
+              checkboxGroupInput("stnTimeseriesType", "Sample Types", c("Resample", "Irregular", "Routine"),
+                                 selected = c("Resample", "Irregular", "Routine")),
               plotOutput("stnTimeseriesChart")
             ),
             tabPanel("Calendar",
+              checkboxGroupInput("stnCalendarType", "Sample Types", c("Resample", "Irregular", "Routine"),
+                                 selected = c("Resample", "Irregular", "Routine")),
               plotOutput("stnCalendarPlot")
             ),
             tabPanel("Histogram",
+              checkboxGroupInput("stnHistogramType", "Sample Types", c("Resample", "Irregular", "Routine"),
+                                 selected = c("Resample", "Irregular", "Routine")),
               plotOutput("stnHistogram")
             ),
             tabPanel("Boxplot",
-              selectInput("stnBoxVariable", "Variable",
-                          choices=c("Select a variable..."="",
-                                    "Water Surface"="WATER_SURFACE",
-                                    "Current Speed"="CURRENT",
-                                    "Tidal Stage"="TIDE",
-                                    "Current Weather"="CURRENT_WEATHER",
-                                    "Past Weather"="PAST_WEATHER")),
+              fluidRow(
+                column(width=6,
+                       selectInput("stnBoxVariable", "Variable",
+                                   choices=c("Select a variable..."="",
+                                             "Water Surface"="WATER_SURFACE",
+                                             "Current Speed"="CURRENT",
+                                             "Tidal Stage"="TIDE",
+                                             "Current Weather"="CURRENT_WEATHER",
+                                             "Past Weather"="PAST_WEATHER"))
+                ),
+                column(width=6,
+                       checkboxGroupInput("stnBoxType", "Sample Types", c("Resample", "Irregular", "Routine"),
+                                          selected = c("Resample", "Irregular", "Routine"))
+                )
+              ),
               plotOutput("stnBoxplot")
             ),
             tabPanel("Scatterplot",
@@ -180,7 +299,12 @@ ui <- dashboardPage(
         )
       ),
       tabItem(tabName = "overview",
-              p("Not implemented yet")),
+        box(width = 12,
+            plotOutput("overResampleBySite")
+        )
+        # box(width = 6,
+        #     plotOutput("overResampleByYear"))
+      ),
       tabItem(tabName = "about",
         box(title = "About", width = 6,
             p("This web application was designed to explore water quality data collected by the ", a(href="http://www.mainehealthybeaches.org/", "Maine Healthy Beaches (MHB) Program"), "."),
@@ -300,8 +424,11 @@ server <- shinyServer(function(input, output, session) {
   }, include.colnames=FALSE, include.rownames=FALSE)
 
   output$stnTimeseriesChart <- renderPlot({
-    x <- stnData()
-    ggplot(x, aes(SAMPLE_DATE, ENTEROCOCCI, color=EXCEED_GRP)) +
+    validate(need(length(input$stnTimeseriesType) > 0, "Select at least one Sample Type above"))
+
+    stnData() %>%
+      filter(SAMPLE_TYPE %in% input$stnTimeseriesType) %>%
+      ggplot(aes(SAMPLE_DATE, ENTEROCOCCI, color=EXCEED_GRP)) +
       geom_point(size = 3, alpha = 0.7) +
       scale_color_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="orangered"), drop=FALSE) +
       scale_y_log10(breaks=log_breaks(seq(1, 9), 10^seq(0, 6)),
@@ -311,28 +438,45 @@ server <- shinyServer(function(input, output, session) {
   })
 
   output$stnHistogram <- renderPlot({
-    x <- stnData()
-    ggplot(x, aes(ENTEROCOCCI, fill=EXCEED_GRP)) +
-      geom_histogram(binwidth=0.1) +
+    # x <- stnData()
+    # ggplot(x, aes(ENTEROCOCCI, fill=EXCEED_GRP)) +
+    #   geom_histogram(binwidth=0.1) +
+    #   scale_fill_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="orangered"), drop=FALSE) +
+    #   scale_x_log10(breaks=log_breaks(seq(1, 9), 10^seq(0, 6)),
+    #                 labels=log_labels(c(1, 5), 10^seq(0, 6))) +
+    #   labs(x="Enterococci (MPN/100mL)", y="# of Samples") +
+    #   theme(axis.text.x = element_text(angle=90, hjust=1, v=0.5),
+    #         panel.grid.minor.y = element_blank())
+    validate(need(length(input$stnHistogramType) > 0, "Select at least one Sample Type above"))
+
+    stnData() %>%
+      filter(SAMPLE_TYPE %in% input$stnHistogramType) %>%
+      mutate(ENTEROCOCCI=ifelse(ENTEROCOCCI > 400, 400, ENTEROCOCCI)) %>%
+      ggplot(aes(ENTEROCOCCI, fill=EXCEED_GRP)) +
+      geom_histogram(binwidth = 10, center = 5) +
       scale_fill_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="orangered"), drop=FALSE) +
-      scale_x_log10(breaks=log_breaks(seq(1, 9), 10^seq(0, 6)),
-                    labels=log_labels(c(1, 5), 10^seq(0, 6))) +
+      scale_x_continuous(breaks=seq(0, 400, 50),
+                         labels=c("0", "50", "100", "150", "200", "250", "300", "350", "400\nor more")) +
+      # scale_x_log10(breaks=log_breaks(seq(1, 9), 10^seq(0, 6)),
+      #               labels=log_labels(c(1, 5), 10^seq(0, 6))) +
       labs(x="Enterococci (MPN/100mL)", y="# of Samples") +
       theme(axis.text.x = element_text(angle=90, hjust=1, v=0.5),
             panel.grid.minor.y = element_blank())
   })
 
   output$stnCalendarPlot <- renderPlot({
+    validate(need(length(input$stnCalendarType) > 0, "Select at least one Sample Type above"))
+
     stnData() %>%
-      mutate(YEAR=year(SAMPLE_DATE),
-             WEEK=as.character(calweek(SAMPLE_DATE)),
-             WEEKDAY=as.character(wday(SAMPLE_DATE))) %>%
+      filter(SAMPLE_TYPE %in% input$stnCalendarType) %>%
       mutate(WEEK=ordered(WEEK, levels=as.character(seq(20, 46))),
              WEEKDAY=plyr::revalue(WEEKDAY, replace = c("1"="Sun", "2"="Mon", "3"="Tue", "4"="Wed", "5"="Thu", "6"="Fri", "7"="Sat"), warn_missing = FALSE),
              WEEKDAY=factor(WEEKDAY, levels=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"))) %>%
       ggplot(aes(WEEK, WEEKDAY, fill=EXCEED_GRP)) +
       geom_tile() +
-      scale_fill_manual("", values=c("<70"="grey50", "70-104"="deepskyblue", ">104"="orangered"), drop=FALSE) +
+      geom_tile(aes(color=SAMPLE_TYPE), fill=NA, size=1) +
+      scale_fill_manual("Ent Category", values=c("<70"="grey70", "70-104"="deepskyblue", ">104"="orangered"), drop=FALSE) +
+      scale_color_manual("Sample Type", values=c("Regular"="white", "Resample"="grey20", "Irregular"="olivedrab3"), drop=FALSE) +
       scale_x_discrete(drop = FALSE) +
       scale_y_discrete(breaks=c("Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"), drop = FALSE) +
       facet_wrap(~YEAR, nrow=5) +
@@ -341,15 +485,18 @@ server <- shinyServer(function(input, output, session) {
   }, height=600)
 
   output$stnBoxplot <- renderPlot({
-    data <- stnData()
     var_x <- input$stnBoxVariable
 
-    validate(need(var_x != "", "Select a variable from the dropdown above."))
+    validate(need(var_x != "", "Select a variable from the dropdown above."),
+             need(length(input$stnBoxType) > 0, "Select at least one Sample Type above"))
 
-    ggplot(data) +
+    stnData() %>%
+      filter(SAMPLE_TYPE %in% input$stnBoxType) %>%
+      ggplot() +
       aes_string(x=var_x, y="ENTEROCOCCI") +
       geom_boxplot(fill = "grey80") +
       labs(x=variable_labels[[var_x]], y="Enterococci (MPN/100mL)") +
+      scale_x_discrete(drop = FALSE) +
       scale_y_log10(breaks=log_breaks(seq(1, 9), 10^seq(0, 6)),
                     labels=log_labels(c(1, 5), 10^seq(0, 6))) +
       theme(axis.text.x = element_text(angle=90, hjust=1, v=0.5),
@@ -378,6 +525,35 @@ server <- shinyServer(function(input, output, session) {
 
     p
   })
+
+  output$overResampleBySite <- renderPlot({
+    p <- wq.yr.site %>%
+      ggplot(aes(SAMPLE_POINT_NAME, VALUE, fill=VARIABLE)) +
+      geom_bar(stat="identity") +
+      scale_fill_manual("",
+                        values=c("MEAN_N_104"="orangered", "MEAN_N_70"="deepskyblue"),
+                        labels=c("MEAN_N_104"="> 104 (SSM)", "MEAN_N_70"="> 70 (BAV)")) +
+      scale_y_continuous(breaks=seq(0, 6, 1)) +
+      labs(x="Sampling Site", y="Mean # Resamples per Year") +
+      theme(axis.text.x=element_text(angle=90, hjust=1, vjust=0.5))
+    p
+  })
+
+  # output$overResampleByYear <- renderPlot({
+  #   p <- select(wq.yr, SAMPLE_POINT_NAME, YEAR, N_CLEAN, N_70, N_104) %>%
+  #     group_by(YEAR) %>%
+  #     summarise(SUM_N_70=sum(N_70)) %>%
+  #     ungroup %>%
+  #     mutate(YEAR = ordered(YEAR, levels=2015:2006)) %>%
+  #     ggplot(aes(factor(YEAR), SUM_N_70)) +
+  #     geom_bar(fill="deepskyblue", stat="identity") +
+  #     geom_text(aes(label=SUM_N_70), hjust=1, nudge_y=-2, size=8, color="white") +
+  #     geom_hline(yintercept=40.9, linetype="dashed") +
+  #     scale_y_continuous(breaks=seq(0, 70, 5)) +
+  #     coord_flip() +
+  #     labs(x="Year", y="# Additional Resamples")
+  #   p
+  # })
 })
 
 # Run the application
